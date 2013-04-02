a34e53b80dbfef329d713d0fa9afd217ada1d450
Martin Nicholls <streaky@mybrokenlogic.com>
2013-04-02 21:22:29 +0100

    Add support for running in phar, creating a FWDIR const for relative paths etc

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,22 +3,17 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-define("ROOTDIR", __DIR__."/");
+if(!defined("ROOTDIR")) {
+	define("ROOTDIR", __DIR__."/");
+}
+define("FWDIR", __DIR__."/");
 
 require_once(ROOTDIR."config.php");
 if(file_exists(ROOTDIR."config-override.php")) {
 	require_once(ROOTDIR."config-override.php");
 }
 
-require_once(ROOTDIR."classes/github/streaky/framework/autoload.php");
+require_once(FWDIR."classes/github/streaky/framework/autoload.php");
 
-define("SITE_URL", "://{$GLOBALS['config']['site-host']}{$GLOBALS['config']['site-path']}");
-
-\github\streaky\framework\autoload::init(ROOTDIR);
-
-use \github\streaky\template\template as tpl;
-tpl::addPath(ROOTDIR."templates/{$GLOBALS['config']['template']}/");
-
-
-tpl::assign("site-url", SITE_URL);
-tpl::assign("static-url", $GLOBALS['config']['static-url']);
+// Autoloader will always use the first it matches so it's important to use any user overrides (ROOTDIR) first
+\github\streaky\framework\autoload::init(array(ROOTDIR, FWDIR));
