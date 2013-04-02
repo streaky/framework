c6f85159b876f127c33bf41f41da7fdca505e3be
Martin Nicholls <streaky@mybrokenlogic.com>
2013-04-02 21:26:43 +0100

    Rebuild autoload taking into account multiple stacking namespace directories that override each other to support separated framework dir and phar

--- src/classes/github/streaky/framework/autoload.php
+++ src/classes/github/streaky/framework/autoload.php
@@ -6,32 +6,40 @@ class autoloadException extends \Exception {}
 
 class autoload {
 	
-	private static $main_path = false;
-	private static $plugins_path = false;
+	private static $root_dirs = array();
 	
-	public static function init($root_dir) {
-		self::$main_path = "{$root_dir}/classes/";
-		self::$plugins_path = "{$root_dir}/plugins/";
+	public static function init(array $root_dirs) {
+		foreach($root_dirs as $dir) {
+			if(!in_array($dir, self::$root_dirs)) {
+				self::$root_dirs[] = $dir;
+			}
+		}
 		spl_autoload_register("self::loader");
 	}
 	
 	public static function loader($class) {
-		if(self::$main_path === false) {
-			throw new autoloadException("Autoloader not initialized");
+		if(count(self::$root_dirs) == 0) {
+			throw new autoloadException("Autoloader not initialized correctly");
 		}
 		
 		if(preg_match('#^'.preg_quote($GLOBALS['config']['plugin-ns'].'\\', '#').'(.*)$#', $class, $matches)) {
 			$target = $matches[1];
-			if(file_exists(self::$plugins_path."{$target}/class.php")) {
-				require_once(self::$plugins_path."{$target}/class.php");
-				return true;
+			foreach(self::$root_dirs as $dir) {
+				$path = "{$dir}plugins";
+				if(file_exists("{$path}/{$target}/class.php")) {
+					require_once("{$path}/{$target}/class.php");
+					return true;
+				}
 			}
 		}
 		
 		$target = str_replace('\\', '/', $class);
-		if(file_exists(self::$main_path."{$target}.php")) {
-			require_once(self::$main_path."{$target}.php");
-			return true;
+		foreach(self::$root_dirs as $dir) {
+			$path = "{$dir}classes";
+			if(file_exists("{$path}/{$target}.php")) {
+				require_once("{$path}/{$target}.php");
+				return true;
+			}
 		}
 		
 		throw new autoloadException("Couldn't find class");
