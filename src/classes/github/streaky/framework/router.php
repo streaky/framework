f800a2fed5e2dc5e0ffc05727eb90e61ceba42e4
Martin Nicholls <streaky@mybrokenlogic.com>
2013-04-02 21:25:08 +0100

    Add support for '-' in request paths (nicer URLs) - switches it for underscore so both will work. Fixed a major issue found running in new production whereby the site path of '/' will cause all sorts of mess to the $uri var, fixed with preg_replace(). Added some support for error routes.

--- src/classes/github/streaky/framework/router.php
+++ src/classes/github/streaky/framework/router.php
@@ -15,61 +15,91 @@ class router {
 
 		// TODO: this has too much effectively repeated code, split it into a method
 		
-		// we want request uri without GET params on the end
-		$uri = explode("?", $_SERVER['REQUEST_URI'], 2); $uri = $uri[0];
+		try {
 		
-		$routes = array();
-		require_once(ROOTDIR."routes.php");
-		$uri = "/".str_replace($GLOBALS['config']['site-path'], "", $uri);
-		$matches = array();
-		foreach($routes as $route) {
+			// we want request uri without GET params on the end
+			$uri = explode("?", $_SERVER['REQUEST_URI'], 2); $uri = $uri[0];
 			
-			if(preg_match($route['match'], $uri, $matches) && is_callable($route['callback'])) {
-				if(!isset($route['method']) || $_SERVER['REQUEST_METHOD'] == $route['method']) {
-					$response = call_user_func($route['callback'], $matches);
+			$routes = array();
+			require_once(ROOTDIR."routes.php");
+			
+			// Fix for site path being '/' and making a horrible mess...
+			$p = preg_quote($GLOBALS['config']['site-path'], '#');
+			$uri = preg_replace('#^'.$p.'#', '/', $uri);
+			
+			$matches = array();
+			foreach($routes as $route) {
+				
+				if(preg_match($route['match'], $uri, $matches) && is_callable($route['callback'])) {
+					if(!isset($route['method']) || $_SERVER['REQUEST_METHOD'] == $route['method']) {
+						$response = call_user_func($route['callback'], $matches);
+						if($response != false) {
+							page::display($response);
+						}
+						return;
+					}
+				}
+			}
+	
+			// Find a possible plugin name
+			$matches = array();
+			if(preg_match('#^/([a-zA-Z0-9_-]+)/(.{0,})$#U', $uri, $matches)) {
+				$matches[1] = str_replace(array("-"), "_", $matches[1]);
+				$plugin = $matches[1];
+				$pluginUri = "/{$matches[2]}";
+				if(file_exists(ROOTDIR."plugins/{$plugin}/routes.php")) {
+					$pluginRoutes = array();
+					require_once(ROOTDIR."plugins/{$plugin}/routes.php");
+					$matches = array();
+					foreach($pluginRoutes as $route) {
+						if(preg_match($route['match'], $pluginUri, $matches)) {
+							if(!isset($route['method']) || $_SERVER['REQUEST_METHOD'] == $route['method']) {
+								$callback = "{$GLOBALS['config']['plugin-ns']}\\{$plugin}::{$route['callback']}";
+								if(is_callable($callback)) {
+									$response = call_user_func($callback, $matches);
+									if($response != false) {
+										page::display($response);
+									}
+									return;
+								}
+							}
+						}
+					}
+					
+					if(isset($pluginRoutes['__error_404'])) {
+						$detail = array("type" => 404, "uri" => $uri, "request-uri" => $_SERVER['REQUEST_URI']);
+						$response = call_user_func($GLOBALS['config']['plugin-ns'].'\error::'.$pluginRoutes['__error_404']['callback'], $detail);
+						if($response != false) {
+							page::display($response);
+						}
+						return;
+					}
+					
+				}
+			}
+			
+			throw new routerException("No route handlers", 404);
+		
+		} catch(routerException $ex) {
+
+			if(file_exists(ROOTDIR."plugins/error/class.php") && file_exists(ROOTDIR."plugins/error/routes.php")) {
+				$pluginRoutes = array();
+				require_once(ROOTDIR."plugins/error/routes.php");
+				if(isset($pluginRoutes['__error_404'])) {
+					$detail = array("type" => 404, "uri" => $uri, "request-uri" => $_SERVER['REQUEST_URI'], "message" => $ex->getMessage());
+					$response = call_user_func($GLOBALS['config']['plugin-ns'].'\error::'.$pluginRoutes['__error_404']['callback'], $detail);
 					if($response != false) {
 						page::display($response);
 					}
 					return;
 				}
 			}
+		
 		}
-
-		// Find a possible plugin name
-		$matches = array();
-		if(preg_match('#^/([a-zA-Z0-9_]+)/(.{0,})$#U', $uri, $matches)) {
-			$plugin = $matches[1];
-			$pluginUri = "/{$matches[2]}";
-			if(file_exists(ROOTDIR."plugins/{$plugin}/routes.php")) {
-				$pluginRoutes = array();
-				require_once(ROOTDIR."plugins/{$plugin}/routes.php");
-				$matches = array();
-				foreach($pluginRoutes as $route) {
-					if(preg_match($route['match'], $pluginUri, $matches)) {
-						if(!isset($route['method']) || $_SERVER['REQUEST_METHOD'] == $route['method']) {
-							$callback = "{$GLOBALS['config']['plugin-ns']}\\{$plugin}::{$route['callback']}";
-							if(is_callable($callback)) {
-								$response = call_user_func($callback, $matches);
-								if($response != false) {
-									page::display($response);
-								}
-								return;
-							}
-						}
-					}
-				}
-				
-				// TODO: Check for plugin 404 route
-				
-			}
-		}
+		
+		
 
 
-		if(isset($routes['_error404'])) {
-			// use route for 404
-		}
-
-
-		throw new routerException("No matching routes for URI");
+		throw new routerException("No matching routes for URI and no suitable 404 handler");
 	}
 }
